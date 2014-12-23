d1372fe5ed11101b90bd3428c5a461bf11abbf15
Jorge Sáez Gómez <josago@gmail.com>
2014-12-23 21:41:57 +0100

    Cleaned project for final uploading.

--- CSLDA.py
+++ CSLDA.py
@@ -114,7 +114,7 @@ class CSLDA:
             
         return (N_kw, N_dk, N_k, N_d, Z)
     
-    def __gibbs_sampler_fast(self, N_kw, N_dk, N_k, N_d, Z, S, W, J = 1):
+    def __gibbs_sampler(self, N_kw, N_dk, N_k, N_d, Z, S, W, J = 1):
         for d in range(W.shape[0]):
             for i in range(W[d].shape[0]):
                 w     = W[d][i] # Word ID.
@@ -161,57 +161,6 @@ class CSLDA:
                 pT = np.exp(pT - np.amax(pT))
                 
                 k_new   = rd.choice(self.K, p = pT / np.sum(pT))
-                Z[d][i] = k_new
-                
-                # Count update with the new topic assignment:
-                
-                N_k[k_new]     += 1
-                N_kw[k_new, w] += 1
-                N_dk[d, k_new] += 1
-    
-    def __gibbs_sampler(self, N_kw, N_dk, N_k, N_d, Z, S, W):
-        for d in range(W.shape[0]):
-            for i in range(W[d].shape[0]):
-                w     = W[d][i] # Word ID.
-                k_old = Z[d][i] # Old topic assignment.
-
-                # This calculates N without the element (d, i):
-
-                N_k[k_old]     -= 1
-                N_kw[k_old, w] -= 1
-                N_dk[d, k_old] -= 1
-                
-                # Some work can be done outside the main loop (in K) in order to pre-calculate things that will be used afterwards (p1 and p2):
-                
-                # NOTE: This Gibbs sampler is implemented in log-space probabilities, in order to avoid numerical problems.
-                
-                p1 = np.subtract(np.sum(sp.gammaln(N_kw + self.beta), axis = 1), sp.gammaln(N_k + self.W * self.beta))
-                p2 = sp.gammaln(N_dk[d] + self.alpha)
-
-                p    = np.zeros((self.K), dtype = float)
-                p[:] = np.sum(p1) + np.sum(p2)
-                
-                selectW    = np.zeros((self.W), dtype = int)
-                selectW[w] = 1
-
-                for k in range(self.K):
-                    selectK    = np.zeros((self.K), dtype = int)
-                    selectK[k] = 1
-                    
-                    p[k] += np.subtract(np.sum(sp.gammaln(N_kw[k] + selectW + self.beta)), sp.gammaln(N_k[k] + 1 + self.W * self.beta)) - p1[k]
-                    
-                    if self.use_scores:
-                        posterior_mean = float(np.divide(N_dk[d] + selectK, np.repeat(np.reshape(N_d[d], (-1, 1)), self.K, axis = 1)).dot(np.reshape(self.eta, (-1, 1))))
-                    
-                        p[k] += np.log(st.norm.pdf(S[d], posterior_mean, self.sigma) + 1E-100) # The + 1E-100 avoids infinities.
-                        
-                    p[k] += sp.gammaln(N_dk[d, k] + self.alpha + 1) - p2[k]
-                
-                # Change to linear-space probabilities. We can also multiply by a constant (maximum element in p) in order to improve the numerical stability of the results:
-                
-                p = np.exp(p - np.amax(p))
-
-                k_new   = rd.choice(self.K, p = p / np.sum(p))
                 Z[d][i] = k_new
                 
                 # Count update with the new topic assignment:
@@ -268,7 +217,7 @@ class CSLDA:
             
             # Calculate sample:
             
-            self.__gibbs_sampler_fast(N_kw, N_dk, N_k, N_d, Z, Strain, Wtrain)
+            self.__gibbs_sampler(N_kw, N_dk, N_k, N_d, Z, Strain, Wtrain)
             self.__estimate_eta(N_dk, N_d, Strain)
             
             if iteration >= num_burn_in and (iteration - num_burn_in) % num_skip == 0:
@@ -310,7 +259,7 @@ class CSLDA:
                 
                 # Calculate sample:
                 
-                self.__gibbs_sampler_fast(N_kw, N_dk, N_k, N_d, Z, S, W1)
+                self.__gibbs_sampler(N_kw, N_dk, N_k, N_d, Z, S, W1)
 
                 if iteration >= num_burn_in and (iteration - num_burn_in) % num_skip == 0:
                     # Use sample:
@@ -375,16 +324,3 @@ for i in range(W - 30, W):
     print (idx_sort[i], scores[idx_sort[i]])
 
 print cslda.test(Stest, Wtest, num_burn_in, num_skip, num_samples, num_samples)
-
-# Plot the results:
-
-#print perplex
-#print inv_acc
-
-#plt.subplot(2, 1, 1)
-#plt.title("Perplexity")
-#plt.plot(perplex)
-#plt.subplot(2, 1, 2)
-#plt.title("Inverse accuracy")
-#plt.plot(inv_acc)
-#plt.show()
